ecb11adf36f454eadd80a02cf667b4382879a4bb
biruk habte <biruk.habte-ug@aau.edu.et>
2025-04-11 00:06:00 +0300

    Update dependencies

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import {RouterProvider} from "react-router";
+import {router} from "./app.routers.jsx"; 
+import { AuthProvider } from "./features/auth/auth.context.jsx";
+import { InterviewProvider } from "./features/interview/interview.context.jsx";
+
+function App() {
+
+  return (
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router}/>
+      </InterviewProvider>
+    </AuthProvider>
+  )
+}
+
+export default App
+
