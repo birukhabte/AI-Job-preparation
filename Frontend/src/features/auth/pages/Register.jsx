0a135af99e0b034089342a907a63495c7e1feefb
biruk habte <biruk.habte-ug@aau.edu.et>
2025-04-22 06:44:00 +0300

    Update security headers

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,63 @@
+import React,{useState} from 'react'
+import "../styles/auth.form.scss"
+import { useNavigate,Link } from 'react-router';
+import { useAuth } from '../hooks/useAuth';
+
+const Register = () => {
+
+    const navigate = useNavigate();
+
+    const {loading,registerHandle} = useAuth()
+
+    const [username,setUsername] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const submitHandle =async (e) => {
+        e.preventDefault();
+        await registerHandle({username,email,password})
+        navigate("/")
+    }
+
+    if(loading){
+        return <main><h1>Loading...</h1></main>
+    }
+
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Register</h1>
+            <form onSubmit={submitHandle}>
+
+                <div className="input-group">
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        onChange={(e)=>{setUsername(e.target.value)}}
+                        type="text" id="username" name="username" placeholder='Enter username' required/>
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                        type="email" id="email" name="email" placeholder='Enter email address' required/>
+                </div>
+                
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                        type="password" id="password" name="password" placeholder='Enter password' required/>
+                </div>
+
+                <button className='button primary-button'>Register</button>
+            </form>
+
+            <p>Already have an account? <Link to={"/login"}>Login</Link></p>
+        </div> 
+    </main>
+  )
+}
+
+export default Register
+
